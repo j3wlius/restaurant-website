103e3a564c32bfbc57d4fbfa84d88b63f0b1d105
julius-tamale <juliotamex@proton.me>
2024-03-26 19:03:58 +0300

    add event listener on toggle

--- resources/script/script.js
+++ resources/script/script.js
@@ -1,9 +1,34 @@
 'use strict';
 
-// PRELOADER END AFTER DOCUMENT LOAD
-const preloader = document.querySelector('[data-preload]');
+/*
+  === PRELOAD === 
+  - loading stops once document is loaded
+*/
 
-window.addEventListener('load', () => {
-  preloader.classList.add('loaded');
+const preload = document.querySelector('[data-preaload]');
+
+window.addEventListener('load', function () {
+  preload.classList.add('loaded');
   document.body.classList.add('loaded');
 });
+
+/*
+  MULTIPLE EVENT LISTENERS ON ELEMENTS
+*/
+const addEventOnElements = (elements, eventType, callback) => {
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+};
+
+const navBar = document.querySelector('[data-navbar]');
+const navTogglers = document.querySelectorAll('[data-nav-toggler]');
+const overlay = document.querySelector('[data-overlay]');
+
+const toggleNavbar = () => {
+  navBar.classList.toggle('active');
+  overlay.classList.toggle('active');
+  document.body.classList.toggle('nav-active');
+};
+
+addEventOnElements(navTogglers, 'click', toggleNavbar);
